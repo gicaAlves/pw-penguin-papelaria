bc3ca274d9d7a9c6186d435c3330c319843c56d2
gicaAlves <giov.alves@outlook.com>
2021-10-08 23:20:33 -0300

    Criar arquivo de inserir novo usuário

---
+++ src/inserir_usu.php
@@ -0,0 +1,10 @@
+<?php
+$nome = $_POST['txtnome'];
+$email = $_POST['txtemail'];
+$senha = $_POST['txtsenha'];
+$end = $_POST['txtendereco'];
+$cidade = $_POST['txtcidade'];
+$cep = $_POST['txtcep'];
+//a variável global $_POST é usada pois o método do formulário é post.
+
+?>
